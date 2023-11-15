e6a1b7afe344e81191cc47e1377695834112c015
omartehsin1 <omartehsin@gmail.com>
2023-11-15 09:37:25 -0500

    adjusted translations and blog page

--- static/js/blog.js
+++ static/js/blog.js
@@ -40,7 +40,7 @@ function renderBlogResults(blogs) {
         <li class="post">
             <div class="post-container">
                 <div class="text-container">
-                    <div>
+                    <div style="margin-top: 1rem">
                         <a href='${paginatedBlogs[i].href}' class="blog-title">
                             <h2 style="font-size: 2.2rem; font-weight:600;">${paginatedBlogs[i].title}</h2>
                         </a>
