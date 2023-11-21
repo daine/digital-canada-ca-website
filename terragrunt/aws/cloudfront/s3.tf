778648d06fa717b217b871abf25f230aa88749be
omartehsin1 <omartehsin@gmail.com>
2023-11-21 16:08:37 -0500

    added new statement block

--- terragrunt/aws/cloudfront/s3.tf
+++ terragrunt/aws/cloudfront/s3.tf
@@ -13,12 +13,25 @@ data "aws_iam_policy_document" "website_bucket_policy_doc" {
       identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
     }
     actions = [
-      "s3:GetObject",
-      "s3:ListBucket"
+      "s3:GetObject"
 
     ]
     resources = [
       "${each.value.s3_bucket_arn}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [
+      "${each.value.s3_bucket_arn}"
     ]
   }
 
