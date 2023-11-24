bfc6bd84c5a09dcca829272b61889975eb64d59b
Pat Heard <patrick.heard@cds-snc.ca>
2023-11-24 18:56:01 -0500

    fix: add missing ecr:SetRepositoryPolicy action (#79)
    Add a required action to the OIDC IAM role that manages
    the PR review env functions.

--- terragrunt/aws/ecr/iam_oidc.tf
+++ terragrunt/aws/ecr/iam_oidc.tf
@@ -64,7 +64,7 @@ data "aws_iam_policy_document" "lambda_function_manage" {
       "iam:PassRole"
     ]
     resources = [
-      "arn:aws:iam::${var.account_id}:role/pr-review-env"
+      aws_iam_role.pr-review-env-lambda.arn
     ]
   }
 }
@@ -84,6 +84,7 @@ data "aws_iam_policy_document" "ecr_image_manage" {
       "ecr:InitiateLayerUpload",
       "ecr:ListImages",
       "ecr:PutImage",
+      "ecr:SetRepositoryPolicy",
       "ecr:UploadLayerPart"
     ]
     resources = [
