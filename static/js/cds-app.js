b4ee9d51e091a3067256d5dda53fa77dc401fbc4
omartehsin1 <omartehsin@gmail.com>
2024-02-13 11:18:22 -0500

    update

--- static/js/cds-app.js
+++ static/js/cds-app.js
@@ -230,39 +230,39 @@ $(document).ready(function () {
     var formData = new FormData(theForm);
     var pageLanguage = $("html").attr("lang");
 
-    for (const pair of formData.entries()) {
-      console.log(pair[0], pair[1])
-    }
+    // for (const pair of formData.entries()) {
+    //   console.log(pair[0], pair[1])
+    // }
 
     var endpoint =
       "https://dowr6jfsw2.execute-api.ca-central-1.amazonaws.com/production/lever";
 
-    // $.ajax({
-    //   type: "POST",
-    //   url: endpoint,
-    //   data: formData,
-    //   contentType: false,
-    //   processData: false,
-    //   complete: function (r) {
-    //     console.log(r.responseText);
-    //   },
-    //   success: function () {
-    //     if (pageLanguage == "en") {
-    //       window.location.href = "/thank-you/";
-    //     } else {
-    //       window.location.href = "/merci/";
-    //     }
-    //   },
-    //   error: function (xhr, textStatus, errorThrown) {
-    //     console.log("Error", textStatus);
-    //     console.log("Error", errorThrown);
-    //     if (pageLanguage == "en") {
-    //       window.location.href = "/error/";
-    //     } else {
-    //       window.location.href = "/erreur/";
-    //     }
-    //   },
-    // });
+    $.ajax({
+      type: "POST",
+      url: endpoint,
+      data: formData,
+      contentType: false,
+      processData: false,
+      complete: function (r) {
+        console.log(r.responseText);
+      },
+      success: function () {
+        if (pageLanguage == "en") {
+          window.location.href = "/thank-you/";
+        } else {
+          window.location.href = "/merci/";
+        }
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        console.log("Error", textStatus);
+        console.log("Error", errorThrown);
+        if (pageLanguage == "en") {
+          window.location.href = "/error/";
+        } else {
+          window.location.href = "/erreur/";
+        }
+      },
+    });
   });
 
   /**
